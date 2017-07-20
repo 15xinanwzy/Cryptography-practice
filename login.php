3674414e0144b037a48cd91d69b3f0d7b6c55037
wzy <wzy15510321335@163.com>
2017-07-20 23:31:20 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body style="width:1000px;margin:0 auto;padding-top:50px ;font-family:Arial,STKaiti,KaiTi,sans-serif;">
+    <div style="width:100%;height:510px;">
+        <img src="http://resource.haorenao.cn/back.jpg">
+        <div style="font-size:30px;position:absolute;top:250px;width:1000px;color:white;">
+          <form action="logincheck.php" method="post">
+            <div style="position:absolute;left:360px;">
+            用户名：
+            <input type="text" name="username" />
+            </div>
+            <br/>
+            <div style="position:absolute;left:410px;">
+            密码:
+            <input type="password" name="passward" />
+            </div>
+            <br/>
+            <div style="position:absolute;left:530px;top:80px">
+            <input type="submit" name="submit" value="登陆" />
+            <a href="register.php"><style="font-size:20px">注冊</a>
+           </div>
+          </form>
+
+    </div>
+
+</body>
+</html>
