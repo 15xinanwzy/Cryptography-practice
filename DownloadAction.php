bc651aa007ff04daa7d5e2a686321695a6bffa56
wzy <wzy15510321335@163.com>
2017-07-27 23:59:30 +0800

    Add files via upload
    触发下载的函数

---
+++ DownloadAction.php
@@ -0,0 +1,38 @@
+<?php
+  header("Content-type:text/html;charset=utf-8");
+  define ('SITE_ROOT', realpath(dirname(__FILE__)));
+
+function download($filename){
+  //用以解决中文不能显示出来的问题
+  $filename=iconv("utf-8","gb2312",$filename);
+
+
+  $file_sub_path=$_SERVER['DOCUMENT_ROOT']."/html/upload/";
+
+  $file_path=$file_sub_path.$filename;
+
+  //首先要判断给定的文件存在与否
+  if(!file_exists($file_path)){
+      echo "没有该文件";
+         return ;
+  }
+
+  $fp=fopen($file_path,"r");
+  $file_size=filesize($file_path);
+
+   //下载文件需要用到的头
+   Header("Content-type: application/octet-stream");
+   Header("Accept-Ranges: bytes");
+   Header("Accept-Length:".$file_size);
+   Header("Content-Disposition: attachment; filename=".$filename);
+   $buffer=1024;
+   $file_count=0;
+   //向浏览器返回数据
+   while(!feof($fp) && $file_count<$file_size){
+       $file_con=fread($fp,$buffer);
+       $file_count+=$buffer;
+       echo $file_con;
+   }
+   fclose($fp);
+}
+?>
